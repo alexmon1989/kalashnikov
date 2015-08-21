f456526b49ec4a8c62327c6e71caa4762c279022
alexmon1989 <alex.mon1989@gmail.com>
2015-08-21 13:32:28 +0300

    1. Переименование "Наши клиенты" в "Наши партнёры" 2. Переход на сайт партнёра.

--- resources/views/marketing/widgets/partners.blade.php
+++ resources/views/marketing/widgets/partners.blade.php
@@ -4,7 +4,7 @@
     @foreach($partners as $partner)
     <div class="item">
     @if(trim($partner->url) != '')
-        <a href="{{ $item->url }}" target="_blank"><img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}"></a>
+        <a title="Перейти на сайт партнёра" href="{{ $partner->url }}" target="_blank"><img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}"></a>
     @else
         <img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}">
     @endif
