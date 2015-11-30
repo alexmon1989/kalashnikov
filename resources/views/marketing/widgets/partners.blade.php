b27586ae1bcb00e5cb23ce11c04aebab41ca3a66
alexmon1989 <alex.mon1989@gmail.com>
2015-11-30 10:01:44 +0200

    Добавление стрелок перематывания в виджет "Наши партнёры".

--- resources/views/marketing/widgets/partners.blade.php
+++ resources/views/marketing/widgets/partners.blade.php
@@ -1,14 +1,24 @@
-<!-- Owl Clients v1 -->
-<div class="headline"><h2>Наши партнёры</h2></div>
-<div class="owl-clients-v1 margin-bottom-20">
-    @foreach($partners as $partner)
-    <div class="item">
-    @if(trim($partner->url) != '')
-        <a title="Перейти на сайт партнёра" href="{{ $partner->url }}" target="_blank"><img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}"></a>
-    @else
-        <img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}">
-    @endif
+<!-- Наши партнёры -->
+<div class="owl-carousel-v1 owl-work-v1 margin-bottom-40">
+    <div class="headline"><h2 class="pull-left">Наши партнёры</h2>
+        <div class="owl-navigation">
+            <div class="customNavigation">
+                <a class="owl-btn prev-clients"><i class="fa fa-angle-left"></i></a>
+                <a class="owl-btn next-clients"><i class="fa fa-angle-right"></i></a>
+            </div>
+        </div><!--/navigation-->
     </div>
-    @endforeach
+
+    <div class="owl-clients-v1">
+        @foreach($partners as $partner)
+            <div class="item">
+                @if(trim($partner->url) != '')
+                    <a title="Перейти на сайт партнёра" href="{{ $partner->url }}" target="_blank"><img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}"></a>
+                @else
+                    <img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}">
+                @endif
+            </div>
+        @endforeach
+    </div>
 </div>
-<!-- End Owl Clients v1 -->
+<!-- End Наши партнёры -->
