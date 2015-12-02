604d23fec34b81a8a6a9bb0bfa7142a17222614d
alexmon1989 <alex.mon1989@gmail.com>
2015-12-02 14:59:01 +0200

    fix: Изменение расположения стрелок движения в виджете "Наши партнёры".

--- resources/views/marketing/widgets/partners.blade.php
+++ resources/views/marketing/widgets/partners.blade.php
@@ -1,24 +1,24 @@
 <!-- Наши партнёры -->
-<div class="owl-carousel-v1 owl-work-v1 margin-bottom-40">
-    <div class="headline"><h2 class="pull-left">Наши партнёры</h2>
-        <div class="owl-navigation">
-            <div class="customNavigation">
-                <a class="owl-btn prev-clients"><i class="fa fa-angle-left"></i></a>
-                <a class="owl-btn next-clients"><i class="fa fa-angle-right"></i></a>
-            </div>
-        </div><!--/navigation-->
+<div class="headline"><h2>Наши партнёры</h2></div>
+<div class="row our-partners">
+    <div class="col-xs-1 text-right">
+       <a class="owl-btn prev-clients"><i class="fa fa-angle-left"></i></a>
     </div>
-
-    <div class="owl-clients-v1">
-        @foreach($partners as $partner)
-            <div class="item">
-                @if(trim($partner->url) != '')
-                    <a title="Перейти на сайт партнёра" href="{{ $partner->url }}" target="_blank"><img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}"></a>
-                @else
-                    <img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}">
-                @endif
-            </div>
-        @endforeach
+    <div class="col-xs-10">
+        <div class="owl-clients-v1 margin-bottom-20">
+            @foreach($partners as $partner)
+                <div class="item">
+                    @if(trim($partner->url) != '')
+                        <a title="Перейти на сайт партнёра" href="{{ $partner->url }}" target="_blank"><img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}"></a>
+                    @else
+                        <img src="{{ asset('img/partners/'.$partner->file_name) }}" alt="{{ $partner->title }}">
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <div class="col-xs-1 text-left">
+       <a class="owl-btn next-clients"><i class="fa fa-angle-right"></i></a>
     </div>
 </div>
 <!-- End Наши партнёры -->
