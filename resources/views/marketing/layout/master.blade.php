ab6e2ab580d85dd3cae17abbbdd4840ad6027ec9
Alexandr Monastyretsky <alex.mon1989@gmail.com>
2016-01-17 12:52:42 +0200

    feature: Добавлена Яндекс.Метрика.

--- resources/views/marketing/layout/master.blade.php
+++ resources/views/marketing/layout/master.blade.php
@@ -119,5 +119,11 @@
 
 @yield('scripts')
 
+@if(preg_match('/vladikavkaz.kalashnikovcom.ru/i', url()))
+<!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter34738380 = new Ya.Metrika({ id:34738380, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/34738380" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+@elseif(preg_match('/krasnodar.kalashnikovcom.ru/i', url()))
+<!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter34738440 = new Ya.Metrika({ id:34738440, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/34738440" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+@endif
+
 </body>
 </html>
