08dbfb7bce05bd3327808aaadd6b3fc6fadc45a3
alexmon1989 <alex.mon1989@gmail.com>
2016-01-21 15:48:27 +0200

    fix: Возможность добавления русскояз. url в модуле "Наши партнёры".

--- app/Http/Requests/StorePartnersRequest.php
+++ app/Http/Requests/StorePartnersRequest.php
@@ -1,10 +1,12 @@
 <?php namespace App\Http\Requests;
 
+use Illuminate\Support\Facades\Validator;
+
 class StorePartnersRequest extends Request {
 
     protected $rules = [
         'title' => 'required|max:255',
-        'url' => 'url|required|max:255',
+        'url' => 'my_url|required|max:255',
         'file_name' => 'image',
         'enabled' => 'boolean',
     ];
@@ -26,6 +28,18 @@ class StorePartnersRequest extends Request {
 	 */
 	public function rules()
 	{
+        \Validator::extend('my_url', function($attribute, $value) {
+            // Длинная регулярка для валидации всевозможных вариантов URL
+            $regExp = '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})' .
+                '(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])' .
+                '(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.' .
+                '(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)' .
+                '(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))' .
+                '(?::\d{2,5})?(?:/[^\s]*)?$_iuS';
+
+            return preg_match($regExp, $value);
+        });
+
         if (Request::segment(3) == 'create')
         {
             $this->rules['file_name'] .= '|required';
