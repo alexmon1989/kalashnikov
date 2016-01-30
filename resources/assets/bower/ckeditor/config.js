7aa6b22b6ad24704b790b2b84d5d5229d5b3e7bd
Alexandr Monastyretsky <alex.mon1989@gmail.com>
2016-01-30 16:58:25 +0200

    fix: Путь к файлам в ckeditor.

--- resources/assets/bower/ckeditor/config.js
+++ resources/assets/bower/ckeditor/config.js
@@ -36,7 +36,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
-    config.filebrowserBrowseUrl = '/kalashnikov/public/elfinder/ckeditor';
+    config.filebrowserBrowseUrl = '/elfinder/ckeditor';
 
     config.extraPlugins = 'youtube';
 
